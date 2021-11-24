cf1fdb16f8e0dbbcdc358d41eb0109295d5deb1b
Okan Karaduman <okankaraduman@192.168.1.41>
2021-11-24 23:07:37 +0300

    restructure directory

--- ders3/loop/main.go
+++ ders3/loop/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	for i, j, s := 0, 5, "a"; i < 3 && j < 100 && s != "aaaaa"; i, j, s = i+1, j+1, s+"a" {
 		fmt.Println("Value of i, j, s:", i, j, s)
 	}
+
+	for _, env := range os.Environ() {
+		fmt.Println(env)
+	}
+
 }
